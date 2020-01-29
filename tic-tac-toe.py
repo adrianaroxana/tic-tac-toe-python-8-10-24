885511091075dbb6b2e50f095bda1f1f03c46bb5
Fazekas Péter <p.fazii888@gmail.com>
2020-01-29 20:39:35 +0100

    unbeatable AI in "HUMAN-AI" game mode

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -36,7 +36,79 @@ def get_move(board, player):
 
 
 def get_ai_move(board, player):
-    """Returns the coordinates of a valid move for player on board."""
+    '''Returns the coordinates of a valid move for player on board.
+    row = random.randint(0, 2)
+    col = random.randint(0, 2)
+    while board[row][col] != 0:
+        row = random.randint(0, 2)
+        col = random.randint(0, 2)
+    return (row, col)
+    '''
+    corners = [board[0][0], board[0][2], board[2][0], board[2][2]]
+    winning_cases = [[board[0][0], board[0][1], board[0][2]], [board[1][0], board[1][1], board[1][2]],
+                     [board[2][0], board[2][1], board[2][2]]]
+    winning_cases2 = [[board[0][0], board[1][0], board[2][0]], [board[0][1], board[1][1], board[2][1]],
+                      [board[0][2], board[1][2], board[2][2]]]
+    winning_cases3 = [board[0][0], board[1][1], board[2][2]]
+    winning_cases4 = [board[0][2], board[1][1], board[2][0]]
+    for i in winning_cases:
+        if sum(i) == 4 and 0 in i:
+            row = board.index(i)
+            col = i.index(0)
+            return(row, col)
+    for i in winning_cases2:
+        if sum(i) == 4 and 0 in i:
+            col = winning_cases2.index(i)
+            row = i.index(0)
+            return(row, col)
+    if sum(winning_cases3) == 4 and 0 in winning_cases3:
+        row = winning_cases3.index(0)
+        col = winning_cases3.index(0)
+        return(row, col)
+    if sum(winning_cases4) == 4 and 0 in winning_cases4:
+        if winning_cases4.index(0) == 1:
+            row, col = 1, 1
+            return (row, col)
+        elif winning_cases4.index(0) == 0:
+            row, col = 0, 2
+            return(row, col)
+        elif winning_cases4.index(0) == 2:
+            row, col = 2, 0
+            return(row, col)
+    for i in winning_cases:
+        if sum(i) == 2 and 1 in i:
+            row = board.index(i)
+            col = i.index(0)
+            return(row, col)
+    for i in winning_cases2:
+        if sum(i) == 2 and 1 in i:
+            col = winning_cases2.index(i)
+            row = i.index(0)
+            return(row, col)
+    if sum(winning_cases3) == 2 and 1 in winning_cases3:
+        row = winning_cases3.index(0)
+        col = winning_cases3.index(0)
+        return(row, col)
+    if sum(winning_cases4) == 2 and 1 in winning_cases4:
+        if winning_cases4.index(0) == 1:
+            row, col = 1, 1
+            return (row, col)
+        elif winning_cases4.index(0) == 0:
+            row, col = 0, 2
+            return(row, col)
+        elif winning_cases4.index(0) == 2:
+            row, col = 2, 0
+            return(row, col)
+    if board[1][1] == 0:
+        row, col = 1, 1
+        return(row, col)
+    if 0 in corners:
+        row = random.randrange(0, 3, 2)
+        col = random.randrange(0, 3, 2)
+        while board[row][col] != 0:
+            row = random.randrange(0, 3, 2)
+            col = random.randrange(0, 3, 2)
+        return(row, col)
     row = random.randint(0, 2)
     col = random.randint(0, 2)
     while board[row][col] != 0:
@@ -112,11 +184,11 @@ def tictactoe_game(mode='HUMAN-HUMAN'):
     if mode == 'HUMAN-HUMAN':
         board = init_board()
         while not has_won(board, 1) and not has_won(board, 2) and not is_full(board):
-            os.system('clear')
+            os.system('cls')
             print_board(board)
             row, col = get_move(board, 1)
             mark(board, 1, row, col)
-            os.system('clear')
+            os.system('cls')
             print_board(board)
             if not has_won(board, 1) and not is_full(board):
                 row, col = get_move(board, 2)
@@ -159,12 +231,18 @@ def tictactoe_game(mode='HUMAN-HUMAN'):
 
 
 def main_menu():
+    '''
+    board = [[2, 2, 2],
+             [2, 0, 0],
+             [2, 2, 2]]
+    get_ai_move(board, 2)
+    '''
     print('''
       ________________   _________   ______   __________  ______
      /_  __/  _/ ____/  /_  __/   | / ____/  /_  __/ __ \/ ____/
-      / /  / // /  ______/ / / /| |/ /  ______/ / / / / / __/   
-     / / _/ // /__/_____/ / / ___ / /__/_____/ / / /_/ / /___   
-    /_/ /___/\____/    /_/ /_/  |_\____/    /_/  \____/_____/  
+      / /  / // /  ______/ / / /| |/ /  ______/ / / / / / __/
+     / / _/ // /__/_____/ / / ___ / /__/_____/ / / /_/ / /___
+    /_/ /___/\____/    /_/ /_/  |_\____/    /_/  \____/_____/
         ''')
     print('1.HUMAN-HUMAN')
     print('2.HUMAN-AI')
