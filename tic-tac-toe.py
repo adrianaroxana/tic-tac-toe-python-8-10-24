5401a29e76120da77911a0cd5e33f3b77f2d79ac
adrianaroxana <60044570+adrianaroxana@users.noreply.github.com>
2020-03-02 13:25:24 +0200

    solved

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,180 +1,135 @@
-import os
+import sys
 import random
-import time
-
+import os
 
 def init_board():
+    """Returns an empty 3-by-3 board (with zeros)."""
     board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     return board
 
 
 def get_move(board, player):
-    row = 3
-    col = 3
-    valid_moves = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']
-    move = input('Make your move: ')
-    if move.lower() == 'quit':
-        quit()
-    elif move.lower() not in valid_moves:
-        print('Not a valid move!')
-        return get_move(board, player)
-    if 'a' in move.lower():
-        row = 0
-    elif 'b' in move.lower():
-        row = 1
-    elif 'c' in move.lower():
-        row = 2
-    if '1' in move:
-        col = 0
-    elif '2' in move:
-        col = 1
-    elif '3' in move:
-        col = 2
-    if board[row][col] != 0:
-        print('Place already taken')
-        return get_move(board, player)
-    return (row, col)
+    """Returns the coordinates of a valid move for player on board."""
+    row, col = 0, 0
+    player_move = input("Choose your move: ").upper()
+    valid_input = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
+    valid_input_length = 2
+    while player_move:
+        if player_move.upper() == "QUIT":
+            sys.exit()
+        elif len(player_move) != valid_input_length: 
+            player_move = input("Please choose a valid move: ").upper()
+        elif player_move not in valid_input:
+            player_move = input("Your coordinates should be between A1 and C3. Please choose a new move ").upper()
+        else:
+            row = ord(player_move[0]) - 65
+            col = ord(player_move[1]) - 49
+            
+            # if 'A' in player_move.upper():
+            #     row = 0
+            # elif 'B' in player_move.upper():
+            #     row = 1
+            # elif 'C' in player_move.upper():
+            #     row = 2
+            # if '1' in player_move:
+            #     col = 0
+            # elif '2' in player_move:
+            #     col = 1
+            # elif '3' in player_move:
+            #     col = 2
+            if board[row][col] != 0:
+                print('Place already taken')
+                return get_move(board, player)
+        return row, col
 
 
 def get_ai_move(board, player):
-    '''
-    row = random.randint(0, 2)
-    col = random.randint(0, 2)
-    while board[row][col] != 0:
-        row = random.randint(0, 2)
-        col = random.randint(0, 2)
-    return (row, col)
-    '''
-    corners = [board[0][0], board[0][2], board[2][0], board[2][2]]
-    winning_cases = [[board[0][0], board[0][1], board[0][2]], [board[1][0], board[1][1], board[1][2]],
-                     [board[2][0], board[2][1], board[2][2]]]
-    winning_cases2 = [[board[0][0], board[1][0], board[2][0]], [board[0][1], board[1][1], board[2][1]],
-                      [board[0][2], board[1][2], board[2][2]]]
-    winning_cases3 = [board[0][0], board[1][1], board[2][2]]
-    winning_cases4 = [board[0][2], board[1][1], board[2][0]]
-    for i in winning_cases:
-        if sum(i) == 4 and 0 in i:
-            row = board.index(i)
-            col = i.index(0)
-            return(row, col)
-    for i in winning_cases2:
-        if sum(i) == 4 and 0 in i:
-            col = winning_cases2.index(i)
-            row = i.index(0)
-            return(row, col)
-    if sum(winning_cases3) == 4 and 0 in winning_cases3:
-        row = winning_cases3.index(0)
-        col = winning_cases3.index(0)
-        return(row, col)
-    if sum(winning_cases4) == 4 and 0 in winning_cases4:
-        if winning_cases4.index(0) == 1:
-            row, col = 1, 1
-            return (row, col)
-        elif winning_cases4.index(0) == 0:
-            row, col = 0, 2
-            return(row, col)
-        elif winning_cases4.index(0) == 2:
-            row, col = 2, 0
-            return(row, col)
-    for i in winning_cases:
-        if sum(i) == 2 and 1 in i:
-            row = board.index(i)
-            col = i.index(0)
-            return(row, col)
-    for i in winning_cases2:
-        if sum(i) == 2 and 1 in i:
-            col = winning_cases2.index(i)
-            row = i.index(0)
-            return(row, col)
-    if sum(winning_cases3) == 2 and 1 in winning_cases3:
-        row = winning_cases3.index(0)
-        col = winning_cases3.index(0)
-        return(row, col)
-    if sum(winning_cases4) == 2 and 1 in winning_cases4:
-        if winning_cases4.index(0) == 1:
-            row, col = 1, 1
-            return (row, col)
-        elif winning_cases4.index(0) == 0:
-            row, col = 0, 2
-            return(row, col)
-        elif winning_cases4.index(0) == 2:
-            row, col = 2, 0
-            return(row, col)
-    if board[1][1] == 0:
-        row, col = 1, 1
-        return(row, col)
-    if 0 in corners:
-        row = random.randrange(0, 3, 2)
-        col = random.randrange(0, 3, 2)
-        while board[row][col] != 0:
-            row = random.randrange(0, 3, 2)
-            col = random.randrange(0, 3, 2)
-        return(row, col)
-    row = random.randint(0, 2)
-    col = random.randint(0, 2)
-    while board[row][col] != 0:
+    """Returns the coordinates of a valid move for player on board."""
+    row, col = 0, 0
+    while board[row][col] != 0:    
         row = random.randint(0, 2)
         col = random.randint(0, 2)
     return (row, col)
 
 
 def mark(board, player, row, col):
-    try:
-        if board[row][col] == 0:
-            board[row][col] = player
-    except IndexError:
-        return
+    """Marks the element at row & col on the board for player."""
+    if board[row][col] == 0:
+        board[row][col] = player
 
 
 def has_won(board, player):
+    """Returns True if player has won the game."""
     if (board[0][0] == board[0][1] == board[0][2] == player or board[1][0] == board[1][1] == board[1][2] == player or
         board[2][0] == board[2][1] == board[2][2] == player or board[0][0] == board[1][0] == board[2][0] == player or
         board[0][1] == board[1][1] == board[2][1] == player or board[0][2] == board[1][2] == board[2][2] == player or
-            board[0][0] == board[1][1] == board[2][2] == player or board[0][2] == board[1][1] == board[2][0] == player):
+        board[0][0] == board[1][1] == board[2][2] == player or board[0][2] == board[1][1] == board[2][0] == player):
         return True
     return False
 
 
 def is_full(board):
-    if 0 in board[0]:
-        return False
-    elif 0 in board[1]:
-        return False
-    elif 0 in board[2]:
-        return False
-    else:
+    """Returns True if board is full."""
+    while (0 not in board[0]) and (0 not in board[1]) and (0 not in board[2]):
         return True
+    return False
 
 
 def print_board(board):
     """Prints a 3-by-3 board on the screen with borders."""
-    newboard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-    for x in range(len(board)):
-        for i in range(len(board[0])):
-            if board[x][i] == 0:
-                newboard[x][i] = '.'
-            if board[x][i] == 1:
-                newboard[x][i] = 'X'
-            if board[x][i] == 2:
-                newboard[x][i] = 'O'
-    print('''
-               1   2   3
-            A  {} | {} | {}
-              ---+---+---
-            B  {} | {} | {}
-              ---+---+---
-            C  {} | {} | {}
-        '''.format(newboard[0][0], newboard[0][1], newboard[0][2], newboard[1][0], newboard[1][1], newboard[1][2],
-                   newboard[2][0], newboard[2][1], newboard[2][2]))
+    newboard = [[0,0,0], [0,0,0], [0,0,0]]
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            if board[row][col] == 0:
+                newboard[row][col] = '.'
+            if board[row][col] == 1:
+                newboard[row][col] = 'X'
+            if board[row][col] == 2:
+                newboard[row][col] = 'O'  
+    print('   1' + '   2' + '   3')
+    print('A  ' + str(newboard[0][0]) + ' | ' + str(newboard[0][1]) + ' | ' + str(newboard[0][2]))
+    print('  ---+---+---')
+    print('B  ' + str(newboard[1][0]) + ' | ' + str(newboard[1][1]) + ' | ' + str(newboard[1][2]))
+    print('  ---+---+---')
+    print('C  ' + str(newboard[2][0]) + ' | ' + str(newboard[2][1]) + ' | ' + str(newboard[2][2]))
 
 
 def print_result(winner):
+    """Congratulates winner or proclaims tie (if winner equals zero)."""
     if winner == 0:
-        print("It's a tie! \n")
+        print('''
+  _____ _______ _  _____                 _______ _____ ______ 
+ |_   _|__   __( )/ ____|       /\      |__   __|_   _|  ____|
+   | |    | |  |/| (___        /  \        | |    | | | |__   
+   | |    | |     \___ \      / /\ \       | |    | | |  __|  
+  _| |_   | |     ____) |    / ____ \      | |   _| |_| |____ 
+ |_____|  |_|    |_____/    /_/    \_\     |_|  |_____|______|
+                                                              
+                                                              
+
+    ''')
     if winner == 1:
-        print('X won! \n')
+        print('''
+    __  __   __        _____  _   _ 
+    \ \/ /   \ \      / / _ \| \ | |
+     \  /     \ \ /\ / / | | |  \| |
+     /  \      \ V  V /| |_| | |\  |
+    /_/\_\      \_/\_/  \___/|_| \_|
+                                
+
+    ''')        
     if winner == 2:
-        print('O won! \n')
+        print('''
+   ___    __          ______  _   _ 
+  / _ \   \ \        / / __ \| \ | |
+ | | | |   \ \  /\  / / |  | |  \| |
+ | | | |    \ \/  \/ /| |  | | . ` |
+ | |_| |     \  /\  / | |__| | |\  |
+  \___/       \/  \/   \____/|_| \_|
+                                   
+                                   
+
+    ''')
 
 
 def tictactoe_game(mode='HUMAN-HUMAN'):
@@ -199,7 +154,7 @@ def tictactoe_game(mode='HUMAN-HUMAN'):
         else:
             winner = 0
         print_result(winner)
-        after_menu()
+        other_menu()
     elif mode == 'HUMAN-AI':
         board = init_board()
         while not has_won(board, 1) and not has_won(board, 2) and not is_full(board):
@@ -222,7 +177,7 @@ def tictactoe_game(mode='HUMAN-HUMAN'):
         else:
             winner = 0
         print_result(winner)
-        after_menu()
+        other_menu()
     elif mode == 'AI-HUMAN':
         board = init_board()
         while not has_won(board, 1) and not has_won(board, 2) and not is_full(board):
@@ -242,50 +197,17 @@ def tictactoe_game(mode='HUMAN-HUMAN'):
         else:
             winner = 0
         print_result(winner)
-        after_menu()
-
-    elif mode == 'AI-AI':
-        board = init_board()
-        while not has_won(board, 1) and not has_won(board, 2) and not is_full(board):
-            row, col = get_ai_move(board, 2)
-            mark(board, 1, row, col)
-            os.system('cls')
-            print_board(board)
-            time.sleep(2)
-            if not has_won(board, 1) and not is_full(board):
-                row, col = get_ai_move(board, 2)
-                mark(board, 2, row, col)
-                os.system('cls')
-                print_board(board)
-                time.sleep(2)
-        if has_won(board, 1):
-            winner = 1
-        elif has_won(board, 2):
-            winner = 2
-        else:
-            winner = 0
-        print_result(winner)
-        after_menu()
+        other_menu()
 
 
-def after_menu():
-    print('1.Play again')
-    print('2.Menu')
-    print('3.Quit')
-    action = input('Play again?')
-    while action != '1' or action != '2' or action != '3':
-        if action == '1':
-            tictactoe_game('AI-AI')
-        elif action == '2':
-            main_menu()
-        elif action == '3':
-            quit()
-        else:
-            action = input('Play again?')
-
+def other_menu():
+    user_answer = input("Do you want to play again?").upper()
+    if user_answer == "YES":
+        main_menu()
+    else:
+        sys.exit()
 
 def main_menu():
-    os.system('cls')
     print('''
       ________________   _________   ______   __________  ______
      /_  __/  _/ ____/  /_  __/   | / ____/  /_  __/ __ \/ ____/
@@ -293,21 +215,22 @@ def main_menu():
      / / _/ // /__/_____/ / / ___ / /__/_____/ / / /_/ / /___
     /_/ /___/\____/    /_/ /_/  |_\____/    /_/  \____/_____/
         ''')
-    print('1.HUMAN-HUMAN')
-    print('2.HUMAN-AI')
-    print('3.AI-HUMAN')
-    print('4.AI-AI')
-    select = input('Choose game mode!')
-    if select == 'quit':
-        quit()
-    elif select == str(1):
+
+    print("Welcome!")
+    print("If you want to play HUMAN-HUMAN press 1")
+    print("If you want to play HUMAN-AI, press 2")
+    print("If you want to play AI-HUMAN, press 3")
+    print("If you want to close the game, type \"quit\"")
+
+    choose = (input('Choose game mode!')).upper()
+    if choose == 'QUIT':
+        sys.exit()
+    elif choose == "1":
         tictactoe_game('HUMAN-HUMAN')
-    elif select == str(2):
+    elif choose == "2":
         tictactoe_game('HUMAN-AI')
-    elif select == str(3):
+    elif choose == "3":
         tictactoe_game('AI-HUMAN')
-    elif select == str(4):
-        tictactoe_game('AI-AI')
     else:
         main_menu()
 
